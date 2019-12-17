8b98936d036df5ade88b6cb29069bc4942f59791
Alison Vieira <alisonvieira.av29@gmail.com>
2019-12-17 14:56:47 -0300

    info basicas

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Olá</h1>
+      <h1>Swapi</h1>
+
+      <p>Informações tiradas deste <a href="https://swapi.co/">site</a></p>
+
+
     </div>
   );
 }

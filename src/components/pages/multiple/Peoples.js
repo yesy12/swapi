82a33d15cb1b941df545f52921be08a9110e99e2
Alison Vieira <alisonvieira.av29@gmail.com>
2019-12-17 15:34:58 -0300

    criado Peoples

---
+++ src/components/pages/multiple/Peoples.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Peoples = () => {
+    return(
+        <div className="Peoples">
+            <h1>Peoples</h1>
+        </div>
+    );
+}
+
+export default Peoples;

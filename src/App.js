c686aacd2c237c0ba81ddd4c419787a3d1116371
Alison Vieira <alisonvieira.av29@gmail.com>
2019-12-17 15:03:45 -0300

    browser router,link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Swapi</h1>
+    <Router>
+      <div className="App">
+        <h1>Swapi</h1>
 
-      <p>Informações tiradas deste <a href="https://swapi.co/">site</a></p>
+        <p>
+          Informações tiradas deste <a href="https://swapi.co" target="_blank">site.</a>
+        </p>
 
-
-    </div>
+      </div>
+    </Router>
   );
 }
 

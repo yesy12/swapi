8bd74598fef18a38cdc8a82a464eae5757f28f95
Alison Vieira <alisonvieira.av29@gmail.com>
2019-12-17 15:11:58 -0300

    rotas

--- src/App.js
+++ src/App.js
@@ -14,14 +14,56 @@ function App() {
         <p>
           Informações tiradas deste <a href="https://swapi.co" target="_blank">site.</a>
         </p>
+      </div>
 
+      <div className="nav">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/peoples">Peoples</Link>
+            </li>
+            <li>
+              <Link to="/planets">Planets</Link>
+            </li>
+            <li>
+              <Link to="/films">Films</Link>
+            </li>            
+            <li>
+              <Link to="/species">Species</Link>
+            </li>            
+            <li>
+              <Link to="/vehicles">Vehicles</Link>
+            </li>
+            <li>
+              <Link to="/starships">
+                Starships
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
 
       <div className="rotas">
-        <Route path="/people">
-          People
+        <Route path="/peoples">
+          Peoples
+        </Route>
+        <Route path="/planets">
+          Planets
+        </Route>
+        <Route path="/films">
+          Films
+        </Route>
+        <Route path="/species">
+          Species
+        </Route>
+        <Route path="/vehicles">
+          Vehicles
+        </Route>
+        <Route path="/starships">
+          Starships
         </Route>
       </div>
+
     </Router>
   );
 }

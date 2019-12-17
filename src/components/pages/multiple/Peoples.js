71cffced79919569d7d793a4eed2889bf99b2ff3
Alison Vieira <alisonvieira.av29@gmail.com>
2019-12-17 15:42:35 -0300

    axios add

--- src/components/pages/multiple/Peoples.js
+++ src/components/pages/multiple/Peoples.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+//import axios from "axios";
 
 const Peoples = () => {
+    const [data,setData] = useState([]);
+
+    useEffect(()=>{
+        setData([4.4,35.34,42.234]);
+    },[setData])
+
     return(
         <div className="Peoples">
             <h1>Peoples</h1>
+            {JSON.stringify(data)}
         </div>
     );
 }
